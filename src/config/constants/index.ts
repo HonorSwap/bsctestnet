856d7b05cec13afdeb52032e158be149a3ccf28e
MEMOPC <memopc@MEMOPC-MacBook-Pro.local>
2021-12-05 11:11:59 +0300

    router

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@honorswap/testnetsdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0xB24cfB6bcDb5a4a658a74ac488BF6d25b25019A8'
 
 // a list of tokens by chain
 type ChainTokenList = {
